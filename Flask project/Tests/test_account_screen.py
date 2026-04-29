4197b13fbd25a43117d6698d3add36c13e16a340
kaydehoopz05 <keanid05@gmail.com>
2026-04-29 18:30:41 +0100

    Added comment

--- Flask project/Tests/test_account_screen.py
+++ Flask project/Tests/test_account_screen.py
@@ -3,7 +3,8 @@ import sys
 import os
 from unittest.mock import patch, MagicMock
 
-# Add the Flask project directory to the Python path
+# Flask directory added to Python path
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
