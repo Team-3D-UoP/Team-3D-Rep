c93dcc783f7112203b6eced324fb7a9e3fdf4942
up2212102 <up2212102@myport.ac.uk>
2026-04-30 17:42:19 +0100

    trying to implement db_chatmanager, getting saving chats

---
+++ Flask project/db_chatManager.py
@@ -0,0 +1,14 @@
+# Stub for db_chatManager module
+# TODO: Implement chat database management functions
+
+def get_chat_messages(user_id):
+    """Get chat messages for a user"""
+    return []
+
+def save_chat_message(user_id, message):
+    """Save a chat message"""
+    pass
+
+def get_chat_history(user_id):
+    """Get chat history for a user"""
+    return []
