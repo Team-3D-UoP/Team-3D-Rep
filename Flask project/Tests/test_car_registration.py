167c7eb30719cf6170afb3bfd5429e8a35b421ac
zahoor <zahoor.uk.edu@gmail.com>
2026-05-01 01:34:13 +0100

    Add tests for car registration page rendering
    - Verify page loads successfully
    - Check page content exists
    - Validate HTML response type

--- Flask project/Tests/test_car_registration.py
+++ Flask project/Tests/test_car_registration.py
@@ -14,3 +14,14 @@ class TestCarRegistration(unittest.TestCase):
         self.app = app
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
+    def test_car_registration_page_loads(self):
+        response = self.client.get('/car_registration')
+        self.assertEqual(response.status_code, 200)
+
+    def test_car_registration_page_has_content(self):
+        response = self.client.get('/car_registration')
+        self.assertGreater(len(response.data), 0)
+
+    def test_car_registration_content_type(self):
+        response = self.client.get('/car_registration')
+        self.assertIn('text/html', response.content_type)
